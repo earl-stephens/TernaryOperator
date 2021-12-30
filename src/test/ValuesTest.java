7aea8124b1ecf6aeb0bb274c6c28732f5429ed81
earl-stephens <stephensearl@hotmail.com>
2021-12-29 23:35:20 -0700

    Test for ternary operator use

--- src/test/ValuesTest.java
+++ src/test/ValuesTest.java
@@ -10,11 +10,28 @@ public class ValuesTest {
 	
 	@Test
 	public void shouldReturnTrue() {
-		App app = new App();
 		Values values = new Values();
-		//app.value = 7;
 		Assert.assertEquals(true, values.testValue());
 		values.setNumberss(6);
 		Assert.assertEquals(false, values.testValue());
+	}
+	
+	@Test
+	public void shouldReturnMinimumValue() {
+		Values values = new Values();
+		int[] inputNumbers = {6, 12, 4, 29, 69};
+		values.setNumArray(inputNumbers);
+		Assert.assertEquals(4, values.getMin());
+		Assert.assertNotEquals(12, values.getMin());
+	}
+	
+	
+	@Test
+	public void shouldReturnMaximumValue() {
+		Values values = new Values();
+		int[] inputNumbers = {6, 12, 4, 29, 69};
+		values.setNumArray(inputNumbers);
+		Assert.assertEquals(69, values.getMax());
+		Assert.assertNotEquals(12, values.getMax());
 	}
 }
